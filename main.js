d0d89b9f0e4b65b0436a72c9da6273b148e65fc4
Michał Gawron <michal.gawron98@gmail.com>
2021-03-20 19:07:34 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+alert('Michał życzy smacznej kawusi ale jebać kapusi pozdro 600');
+const firstName ='Michał';
+const age = 69;
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam się ${firstName} i mam ${age} lat`);
---
+++ posejdon.js
@@ -0,0 +1,5 @@
+alert('Miasto 24H robie hahaha');
+const firstName='Patryk';
+const lastName='Wiczyński';
+const age=25;
+console.log('Jestem ${firstName}posejdon${lastName}i wyglądam w chuj staro ale mam${age}');
